e0697523f7ca9a2efce3661e31018ae77c27cf5f
k-romero <imkevinromero@gmail.com>
2020-03-07 13:47:41 -0500

    fixed compile errors and created method stubs to satisfy the test parameters

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,27 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<E> {
+
+    E[] arrToWorkWith;
+
+    public ArrayUtility(E[] arrToWorkWith) {
+        this.arrToWorkWith = arrToWorkWith;
+    }
+
+    public Integer countDuplicatesInMerge(E[] arrayToMerge, E valueToEvaluate) {
+        return null;
+    }
+
+    public E getMostCommonFromMerge(E[] arrayToMerge) {
+        return null;
+    }
+
+    public E[] removeValue(E valueToRemove) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(E valueToEvaluate) {
+        return null;
+    }
 }
